2464a129a379f46ae3ca9698eed5c687dbf20cd7
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-22 12:52:40 -0500

    implement styled-components in Tab

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const TabDiv = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+
+  ${props => props.active && css`
+    background-color: #fff;
+    color: #333;
+    border: 2px solid #333;
+  `}
+`;
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
@@ -7,8 +32,8 @@ const Tab = props => {
       if it is not it should just be 'tab'*/
   const { tab, selectTabHandler, selectedTab } = props;
   return (
-    <div
-      className={selectedTab === tab && 'active-tab'}
+    <TabDiv
+      active={selectedTab === tab}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props
          you'll need to pass the `tab` in as an argument to this handler. */
@@ -17,7 +42,7 @@ const Tab = props => {
       }}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabDiv>
   );
 };
 
