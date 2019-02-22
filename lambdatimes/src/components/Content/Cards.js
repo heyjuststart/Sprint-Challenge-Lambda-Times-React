55e3efae7e66e9feb3c1d59e14a8cc4280659fcc
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-02-22 13:03:45 -0500

    implement styled-components in Cards/Card
    Took out the index.css import as well

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import Card from './Card';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Card from './Card';
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = ({ cards }) => {
   return (
-    <div className="cards-container">
+    <CardsContainer>
       {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
       {cards.map(c => (
         <Card card={c} key={c.headline} />
       ))}
-    </div>
+    </CardsContainer>
   );
 };
 
